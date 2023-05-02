01f1c083043b91d4e862400262a73107e03a6e6e
Caussz <cauayt2510062gmail.com>
2023-05-02 11:47:21 -0300

    funcional

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,12 @@
 <script setup>
-  import { ref } from 'vue'
-
+  import { ref } from 'vue';
+  import {produtos} from './utils/produtos';
   const carrinho = ref([]);
+  const precos = ref()
+  const mostrarC = ref(false)
+  const itemc = ref([])
 
-  const produtos = [
-    {
-        id: 1,
-        nome: 'Camiseta',
-        preco: 49.90
-    },
-    {
-        id: 2,
-        nome: 'Calça',
-        preco: 99.90
-    },
-    {
-        id: 3,
-        nome: 'Meia',
-        preco: 9.90
-    },
-    {
-        id: 4,
-        nome: 'Sapato',
-        preco: 199.90
-    },
-    {
-        id: 5,
-        nome: 'Boné',
-        preco: 29.90
-    },
-    {
-        id: 6,
-        nome: 'Óculos',
-        preco: 99.90
-    },
-    {
-        id: 7,
-        nome: 'Relógio',
-        preco: 299.90
-    },
-    {
-        id: 8,
-        nome: 'Bermuda',
-        preco: 79.90
-    },
-    {
-        id: 9,
-        nome: 'Cueca',
-        preco: 19.90
-    },
-    {
-        id: 10,
-        nome: 'Meia',
-        preco: 9.90
-    }
-]
+  
 
   const novoItem = ref({
     id: 0,
@@ -66,7 +18,7 @@
     novoItem.value.id = produtos[id].id;
     novoItem.value.produto = produtos[id].nome;
     novoItem.value.preco = produtos[id].preco;
-    carrinho.value.push(novoItem.value)
+    carrinho.value.push({...novoItem.value})
     novoItem.value.id = 0;
     novoItem.value.produto = '';
     novoItem.value.preco = 0;
@@ -76,15 +28,29 @@
   function remover(index) {
     carrinho.value.splice(index, 1)
   }
+
+
+  for (precos.value of carrinho.value) {
+    console.log(precos)
+    let totals = precos.value.preco += precos.value.preco
+    console.log(totals)
+  }
+
 </script>
 <template>
   <input type="text" v-model="novoItem.produto">
-  <button @click="adicionar(2)">Adicionar</button>
+  <button @click="adicionar()">Adicionar</button>
   <button @click="remover">remover</button>
   <ul>
-    <li v-for="(item, index) of produtos" @click="adicionar(index)">{{ JSON.stringify(item.nome) }}</li>
+    <li v-for="(item, index) of produtos"> <p @click="adicionar(index)">add</p> <p @click="remover(index)">remover</p>{{ JSON.stringify(item.nome) }}</li>
   </ul>
-  {{ carrinho }}
+  <button @click="mostrarC = true">CArrinho</button>
+
+  <div v-if="mostrarC">
+    <li v-for="(itemc, index) of carrinho"> <p @click="adicionar(index)">add</p> <p @click="remover(index)">remover</p>{{ JSON.stringify(itemc.nome) }}</li>
+    <button @click="mostrarC = false">produtos</button>
+
+  </div>
 </template>
 
 
