51fc54f5e0e339fc7f82b1c078c0715424911f97
Caussz <cczcaua@gmail.com>
2023-05-04 17:09:02 -0300

    funcional e comeco do css

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <script setup>
-import { ref } from 'vue';
-import { produtos } from './utils/produtos';
+import { ref } from "vue";
+import { produtos } from "./utils/produtos";
 
 const carrinho = ref([]);
 const totalProdutos = ref(0);
 const totalPreco = ref(0);
 const mostrarCarrinho = ref(true);
-const mostrar = ref(true)
+const mostrar = ref(false);
 
 const novoItem = ref({
   id: 0,
-  nome: '',
+  nome: "",
   preco: 0,
+  img: "",
+  desc: "",
 });
 
-function adicionar(index) {
+function adicionar(id) {
+  const produtoSelecionado = produtos.find((produto) => produto.id === id);
+  novoItem.value.id = produtoSelecionado.id;
+  novoItem.value.nome = produtoSelecionado.nome;
+  novoItem.value.preco = produtoSelecionado.preco;
+  novoItem.value.img = produtoSelecionado.img;
+  novoItem.value.desc = produtoSelecionado.desc;
+  carrinho.value.push({ ...novoItem.value });
+  novoItem.value.id = 0;
+  novoItem.value.nome = "";
+  novoItem.value.preco = 0;
+  calcularTotal();
+}
+
+function limparCarrinho() {
+  carrinho.value = [];
+  calcularTotal();
+}
+
+function verMais(index) {
   const produtoSelecionado = produtos[index];
   novoItem.value.id = produtoSelecionado.id;
   novoItem.value.nome = produtoSelecionado.nome;
   novoItem.value.preco = produtoSelecionado.preco;
-  carrinho.value.push({ ...novoItem.value });
-  novoItem.value.id = 0;
-  novoItem.value.nome = '';
-  novoItem.value.preco = 0;
-  calcularTotal();
+  novoItem.value.img = produtoSelecionado.img;
+  novoItem.value.desc = produtoSelecionado.desc;
+  mostrar.value = !mostrar.value;
 }
 
 function remover(index) {
@@ -37,56 +56,322 @@ function calcularTotal() {
 }
 </script>
 <template>
-  <div class="container" v-if="mostrarCarrinho">
-    <div class="header">
-      <h1>Produtos</h1>
-      <button class="cart-button" @click="mostrarCarrinho = false">
-        Carrinho ({{ totalProdutos }})
-      </button>
-    </div>
+  <header v-if="mostrarCarrinho && mostrar == false">
+    <h1>Produtos</h1>
+    <button class="cart-button" @click="mostrarCarrinho = false">
+      Meu carrinho ({{ totalProdutos }})
+    </button>
+  </header>
+  <div v-if="mostrar">
+    <main class="container">
+      <div class="produto-info">
+        <img :src="novoItem.img" :alt="novoItem.nome" />
+        <h2>{{ novoItem.nome }}</h2>
+        <p>{{ novoItem.desc }}</p>
+        <p>R$ {{ novoItem.preco.toFixed(2) }}</p>
+      </div>
+      <button class="ver-mais" @click="mostrar = !mostrar">Voltar</button>
+    </main>
+  </div>
+
+  <div class="container" v-else-if="mostrarCarrinho">
     <div class="content">
       <div class="produtos">
         <div v-for="(produto, index) of produtos" :key="produto.id" class="produto">
           <div class="produto-info">
+            <div>
+              <img :src="produto.img" :alt="produto.nome" />
+            </div>
             <h2>{{ produto.nome }}</h2>
             <p>{{ produto.descricao }}</p>
             <p>R$ {{ produto.preco.toFixed(2) }}</p>
           </div>
+          <button class="remove-button" @click="remover(index)">Remover</button>
           <button class="add-button" @click="adicionar(index)">Adicionar</button>
+          <button class="ver-mais" @click="verMais(index)">Mais info...</button>
         </div>
       </div>
     </div>
   </div>
   <div class="carrinho" v-else>
     <div class="carrinho-header">
-      <h2>Carrinho ({{ totalProdutos }})</h2>
-      <button class="close-button" @click="mostrarCarrinho = true">
-        Fechar
-      </button>
+      <h2>Total de itens no carrinho: {{ totalProdutos }}</h2>
     </div>
-    <div class="carrinho-items">
-      <div v-if="carrinho.length === 0">
-        <p>Nenhum produto no carrinho.</p>
-      </div>
-      <div v-else>
-        <div v-for="(item, index) of carrinho" :key="item.id" class="carrinho-item">
-          <div class="carrinho-item-info">
-            <h3>{{ item.nome }}</h3>
-            <p>R$ {{ item.preco.toFixed(2) }}</p>
+    <main class="container">
+      <div class="carrinho-items">
+        <div v-if="carrinho.length == 0">
+          <p>Nenhum produto no carrinho.</p>
+        </div>
+        <div v-else>
+          <div v-for="(item, index) of carrinho" :key="item.id" class="carrinho-item">
+            <div class="carrinho-item-info">
+              <h3>{{ item.nome }}</h3>
+              <p>R$ {{ item.preco.toFixed(2) }}</p>
+            </div>
+            <button class="remove-button" @click="remover(index)">Remover</button>
+            <button class="add-button" @click="adicionar(item.id)">Adicionar</button>
           </div>
-          <button class="remove-button" @click="remover(index)">Remover</button>
-        </div>
-        <div class="carrinho-total">
-          <p>Total: R$ {{ totalPreco.toFixed(2) }}</p>
+          <div class="carrinho-total">
+            <p>Total: R$ {{ totalPreco.toFixed(2) }}</p>
+            <button class="remove-button" @click="limparCarrinho">Limpar carrinho</button>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
+    <button class="close-button" @click="mostrarCarrinho = true">Voltar ao inicio</button>
   </div>
 </template>
 
-
 <style scoped>
-li{
-  background-color: aqua;
+main.container {
+  background-color: #d6c292e8;
+  min-width: 320px;
+  min-height: 40vh;
+  padding: 2rem;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+}
+
+main h2 {
+  font-weight: 600;
+  margin-bottom: 2rem;
+  position: relative;
+}
+
+img {
+  max-width: 200px;
+  max-height: 280px;
+  border-radius: 5px;
+}
+.container {
+  width: 80%;
+  margin: auto;
+  padding: 20px;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #d6c292e8;
+  padding: 10px;
+}
+
+.cart-button {
+  background-color: transparent;
+  border: 2px solid #fff;
+  border-radius: 4px;
+  color: #fff;
+  font-size: 1rem;
+  padding: 0.5rem;
+  cursor: pointer;
+  margin: 10px;
+  transition: background-color 0.2s, color 0.2s;
+}
+
+.cart-button:hover {
+  background-color: #fff;
+  color: #3d2e1e;
+}
+.content {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.produtos {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.produto {
+  width: calc(33.33% - 20px);
+  margin: 10px;
+  padding: 20px;
+  border-radius: 5px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #d6c292;
+}
+
+.produto-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.produto-info img {
+  width: 100%;
+  max-width: 400px;
+  height: auto;
+  margin-bottom: 1rem;
+}
+
+.produto-info h2 {
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
+}
+
+.produto-info p {
+  margin-bottom: 1rem;
+}
+
+.produto-info p:last-of-type {
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.add-button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.ver-mais {
+  background-color: #3d2e1e;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  padding: 0.5rem;
+  margin-top: 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s, color 0.2s;
+}
+
+.ver-mais:hover {
+  background-color: #fff;
+  color: #3d2e1e;
+}
+
+.carrinho {
+  margin: auto;
+  padding: 30px;
+}
+
+.carrinho-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 30px;
+}
+
+.close-button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 30px;
+}
+
+.carrinho-items {
+  margin-top: 20px;
+}
+
+.carrinho-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.carrinho-item-info {
+  text-align: center;
+}
+
+.remove-button {
+  background-color: #dc3545;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.carrinho-total {
+  margin-top: 20px;
+  text-align: right;
+}
+
+@media (max-width: 480px) {
+  main.container {
+    min-height: 50vh;
+    padding: 1rem;
+  }
+
+  main h2 {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  img {
+    max-width: 100%;
+    max-height: auto;
+    margin-bottom: 1rem;
+  }
+
+  .container {
+    padding: 10px;
+  }
+
+  header {
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100vh;
+  }
+
+  .cart-button {
+    margin-top: 1rem;
+  }
+
+  .produtos {
+    flex-direction: column;
+  }
+
+  .produto {
+    width: 100%;
+    margin: 10px 0;
+    padding: 10px;
+  }
+
+  .add-button,
+  .ver-mais {
+    width: 100%;
+  }
+
+  .carrinho {
+    padding: 10px;
+  }
+
+  .carrinho-header {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .close-button {
+    margin-top: 1rem;
+  }
+
+  .carrinho-item {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .remove-button {
+    margin-top: 1rem;
+  }
+
+  .carrinho-total {
+    margin-top: 1rem;
+    text-align: left;
+  }
 }
 </style>
--- src/utils/produtos.js
+++ src/utils/produtos.js
@@ -2,51 +2,64 @@ export const produtos = [
     {
         id: 1,
         nome: 'Camiseta',
-        preco: 49.90
+        preco: 49.90,
+        img: 'https://images.tcdn.com.br/img/img_prod/809258/camiseta_preta_malha_pv_manga_curta_gola_redonda_267_1_691009fed2c687cf9c88e74f6a4ebbf3.jpg',
+        desc: 'gggg'
     },
     {
         id: 2,
         nome: 'Calça',
-        preco: 99.90
+        preco: 99.90,
+        img: 'https://images.tcdn.com.br/img/img_prod/747002/calca_masculina_jogger_cargo_sarja_hoshwear_areia_951_1_9e83c4e768cf9dc80e4dccdf46f532bc_20220718102059.jpg',
+        desc: 'eeeee'
     },
     {
         id: 3,
         nome: 'Meia',
-        preco: 9.90
+        preco: 9.90,
+        img: 'https://static.netshoes.com.br/produtos/meia-cano-medio-puma-logo-c-3-pares/58/D14-7141-158/D14-7141-158_zoom1.jpg?ts=1570543225',
+        desc: 'yyyy'
     },
     {
         id: 4,
         nome: 'Sapato',
-        preco: 199.90
+        preco: 199.90,
+        img: 'https://static.netshoes.com.br/produtos/sapato-casual-masculino-loafer-elite-couro-premium/38/OVM-0265-138/OVM-0265-138_zoom1.jpg?ts=1644417041',
+        desc: 'uuuu'
     },
     {
         id: 5,
         nome: 'Boné',
-        preco: 29.90
+        preco: 29.90,
+        img: 'https://images-americanas.b2w.io/produtos/3288100069/imagens/bone-ny-new-york-strapback-yankees-bordado-fitao-qualidade-premium-padrao-original/3288100077_1_large.jpg',
+        desc: 'iiiii'
     },
     {
         id: 6,
         nome: 'Óculos',
-        preco: 99.90
+        preco: 99.90,
+        img: 'https://cdn.shopify.com/s/files/1/0402/8098/1658/products/17fb5a443f38566a149dc60a6f3d6741.jpg?v=1633623692',
+        desc: 'ffff'
     },
     {
         id: 7,
         nome: 'Relógio',
-        preco: 299.90
+        preco: 299.90,
+        img: 'https://adesimais.com.br/15409-large_default/relogio-de-pulso-personalizado.jpg',
+        desc: 'aaaaaa'
     },
     {
         id: 8,
         nome: 'Bermuda',
-        preco: 79.90
+        preco: 79.90,
+        img: 'https://images.tcdn.com.br/img/img_prod/652999/bermuda_cargo_moletom_preta_box_logo_4713_1_692c62e44718f74a18bf45a31458ffd9.jpg',
+        desc: 'bbbbb'
     },
     {
         id: 9,
         nome: 'Cueca',
-        preco: 19.90
-    },
-    {
-        id: 10,
-        nome: 'Meia',
-        preco: 9.90
+        preco: 19.90,
+        img: 'https://cdn.dooca.store/29563/products/d6yz44pifk1xlly2qcegtq84zby46drptvbw_640x640+fill_ffffff.jpg?v=1663102131&webp=0',
+        desc: 'ccccc'
     }
 ]
